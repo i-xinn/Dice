89b90ecb276908fc2fd8bfd540836421857b814b
Nitin <123466788+i-xinn@users.noreply.github.com>
2023-03-04 14:15:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let randomNumber1 = Math.floor((Math.random() * 6)) + 1;
+let randomNumber2 = Math.floor((Math.random() * 6)) + 1;
+
+let randomImg1 = "images/dice" + randomNumber1 + ".png";
+let randomImg2 = "images/dice" + randomNumber2 + ".png";
+
+let image1 = document.querySelectorAll("img")[0];
+let image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImg1);
+image2.setAttribute("src", randomImg2);
+
+let h1 = document.querySelector("h1");
+
+if (randomNumber1 === randomNumber2) {
+    h1.textContent = "Draw !";
+} else if (randomNumber1 > randomNumber2) {
+    h1.textContent = "🚩 Player 1 Wins !";
+} else {
+    h1.textContent = "Player 2 Wins ! 🚩";
+}
